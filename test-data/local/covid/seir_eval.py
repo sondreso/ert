9e3541e3590fac9da2de9899f4d3ac948a52bb0e
Sondre Sortland <sonso@equinor.com>
2020-10-17 21:36:07 +0200

    Add data for norway

--- test-data/local/covid/seir_eval.py
+++ test-data/local/covid/seir_eval.py
@@ -12,22 +12,36 @@ WD = os.path.dirname(__file__)
 
 
 @click.command()
-@click.option('--config_file', '-cf',
-              type=click.Path(exists=True),
-              help='Path to config ini file.')
-@click.option('--contacts_matrix_file', '-cm',
-              type=click.Path(exists=True),
-              help='Path to contact matrix file')
-@click.option('--visualize-compartments', '-vc',
-              type=bool,
-              is_flag=True)
-@click.option('--output_file_format', '-of',
-              type=click.Choice(['json', 'csv']), default='csv',
-              help='Output file format')
-@click.option('--output_file', '-o', default="out.csv",
-              type=str,
-              help='Path to output file.')
-def main(config_file, contacts_matrix_file, visualize_compartments, output_file_format, output_file):
+@click.option(
+    "--config_file",
+    "-cf",
+    type=click.Path(exists=True),
+    help="Path to config ini file.",
+)
+@click.option(
+    "--contacts_matrix_file",
+    "-cm",
+    type=click.Path(exists=True),
+    help="Path to contact matrix file",
+)
+@click.option("--visualize-compartments", "-vc", type=bool, is_flag=True)
+@click.option(
+    "--output_file_format",
+    "-of",
+    type=click.Choice(["json", "csv"]),
+    default="csv",
+    help="Output file format",
+)
+@click.option(
+    "--output_file", "-o", default="out.csv", type=str, help="Path to output file."
+)
+def main(
+    config_file,
+    contacts_matrix_file,
+    visualize_compartments,
+    output_file_format,
+    output_file,
+):
     """Console script for SEIR."""
     # TODO: Handle somehow the creation of an imports function
     # Setup the model
@@ -48,21 +62,29 @@ def main(config_file, contacts_matrix_file, visualize_compartments, output_file_
     model.simulate(**sim_kwargs)
 
     # Evaluate the solution
-    time = np.arange(0, sim_kwargs['max_simulation_time'], 1, dtype=int)
+    time = np.arange(0, sim_kwargs["max_simulation_time"], 1, dtype=int)
     results = model.evaluate_solution(time)
     # Save data
-    if output_file_format == 'csv':
-        output_file = output_file or os.path.basename(f'{config_file}.csv')
+    if output_file_format == "csv":
+        output_file = output_file or os.path.basename(f"{config_file}.csv")
         results.to_csv(output_file)
-    elif output_file_format == 'json':
-        output_file = output_file or os.path.basename(f'{config_file}.json')
+    elif output_file_format == "json":
+        output_file = output_file or os.path.basename(f"{config_file}.json")
         results.to_json(output_file)
 
-    hospitalized = results["hospitalized (active)"].fillna(0).values.tolist()
     def convert_num(a):
         return str(int(round(a)))
-    with open('hospitalized_0.out', 'w') as f:
-        f.write('\n'.join(map(convert_num, hospitalized)))
+
+    def export_column(column_name, file_name):
+        hospitalized = results[column_name].fillna(0).values.tolist()
+        with open(file_name, "w") as f:
+            f.write("\n".join(map(convert_num, hospitalized)))
+
+    export_column("infected (active)", "infected_0.out")
+    export_column("hospitalized (active)", "hospitalized_0.out")
+    export_column("in ICU", "intensive_0.out")
+    export_column("removed", "dead_0.out")
+
 
 if __name__ == "__main__":
     main()
