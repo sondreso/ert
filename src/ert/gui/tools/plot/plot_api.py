bbcae6575a1454029b6d1c556e701c0d267b9513
Sondre Sortland <sonso@equinor.com>
2023-10-27 07:23:18 +0200

    Working poly and snake_oil

--- src/ert/gui/tools/plot/plot_api.py
+++ src/ert/gui/tools/plot/plot_api.py
@@ -17,25 +17,21 @@ logger = logging.getLogger(__name__)
 
 class PlotApi:
     def __init__(self, storage):
-        print("Called: __init__")
         self._all_cases: List[dict] = None
         self._timeout = 120
         self._storage: StorageReader = storage
         self._reset_storage_facade()
 
     def _reset_storage_facade(self):
-        print("Called: _reset_storage_facade")
         self._storage.refresh()
 
     def _get_case(self, name: str) -> Optional[dict]:
-        print("Called: _get_case")
         for e in self._get_all_cases():
             if e["name"] == name:
                 return e
         return None
 
     def _get_all_cases(self) -> List[dict]:
-        print("Called: _get_all_cases")
         if self._all_cases is not None:
             return self._all_cases
 
@@ -50,34 +46,9 @@ class PlotApi:
                 }
             )
         return self._all_cases
-        # with StorageService.session() as client:
-        #     try:
-        #         response = client.get("/experiments", timeout=self._timeout)
-        #         self._check_response(response)
-        #         experiments = response.json()
-        #         for experiment in experiments:
-        #             for ensemble_id in experiment["ensemble_ids"]:
-        #                 response = client.get(
-        #                     f"/ensembles/{ensemble_id}", timeout=self._timeout
-        #                 )
-        #                 self._check_response(response)
-        #                 response_json = response.json()
-        #                 case_name = response_json["userdata"]["name"]
-        #                 self._all_cases.append(
-        #                     {
-        #                         "name": case_name,
-        #                         "id": ensemble_id,
-        #                         "hidden": case_name.startswith("."),
-        #                     }
-        #                 )
-        #         return self._all_cases
-        #     except IndexError as exc:
-        #         logging.exception(exc)
-        #         raise exc
 
     @staticmethod
     def _check_response(response: requests.Response):
-        print("Called: _check_response")
         if response.status_code != httpx.codes.OK:
             raise httpx.RequestError(
                 f" Please report this error and try restarting the application."
@@ -85,7 +56,6 @@ class PlotApi:
             )
 
     def _get_experiments(self) -> dict:
-        print("Called: _get_experiments")
         with StorageService.session() as client:
             response: requests.Response = client.get(
                 "/experiments", timeout=self._timeout
@@ -94,7 +64,6 @@ class PlotApi:
             return response.json()
 
     def _get_ensembles(self, experiement_id) -> List:
-        print("Called: _get_ensembles")
         with StorageService.session() as client:
             response: requests.Response = client.get(
                 f"/experiments/{experiement_id}/ensembles", timeout=self._timeout
@@ -110,68 +79,84 @@ class PlotApi:
 
         For each key a dict is returned with info about
         the key"""
-        print("Called: all_data_type_keys")
 
         all_keys = {}
 
+        all_parameter_sub_keys = set()
         for experiment in self._storage.experiments:
-            print("experiment.parameter_info:", experiment.parameter_info)
-            print("experiment.response_info:", experiment.response_info)
-            resp_with_obs = [obs.attrs["response"] for obs in experiment.observations.values()]
+            ensembles = list(experiment.ensembles)
+            if len(ensembles) == 0:
+                continue
+            ensemble = ensembles[0]
+            resp_with_obs = [
+                obs.attrs["response"] for obs in experiment.observations.values()
+            ]
             for key, value in experiment.response_info.items():
-                all_keys[key] = {
-                    "key": key,
-                    "index_type": "VALUE",
-                    "observations": key in resp_with_obs,
-                    "dimensionality": 2,
-                    "metadata": {"data_origin": value["_ert_kind"]},
-                    "log_scale": key.startswith("LOG10_"),
-                }
+                if value["_ert_kind"] == "SummaryConfig":
+                    sub_indexes = (
+                        ensemble.load_response(
+                            key, tuple(range(ensemble.ensemble_size))
+                        )
+                        .get_index("name")
+                        .to_list()
+                    )
+                    summary_obs = [
+                        obs.get_index("name").to_list()[0]
+                        for obs in experiment.observations.values()
+                        if obs.attrs["response"] == key
+                    ]
+                    for sub_index in sub_indexes:
+                        comb_key = key + ":" + sub_index
+                        all_keys[comb_key] = {
+                            "key": comb_key,
+                            "index_type": "VALUE",
+                            "observations": sub_index in summary_obs,
+                            "dimensionality": 2,
+                            "metadata": {"data_origin": value["_ert_kind"]},
+                            "log_scale": comb_key.startswith("LOG10_"),
+                        }
+                    pass
+                else:
+                    obs_rep_steps = [
+                        obs.get_index("report_step").to_list()[0]
+                        for obs in experiment.observations.values()
+                        if obs.attrs["response"] == key
+                    ]
+                    rep_steps = (
+                        [0] if value["report_steps"] is None else value["report_steps"]
+                    )  # Hvorfor er obs markert med report_step men ikke med i response?
+                    for rep_step in rep_steps:
+                        all_keys[key] = {
+                            "key": f"{key}@{rep_step}",
+                            "index_type": "VALUE",
+                            "observations": key in resp_with_obs
+                            and rep_step in obs_rep_steps,
+                            "dimensionality": 2,
+                            "metadata": {"data_origin": value["_ert_kind"]},
+                            "log_scale": key.startswith("LOG10_"),
+                        }
             for key, value in experiment.parameter_info.items():
-                all_keys[key] = {
-                    "key": key,
-                    "index_type": None,
-                    "observations": False,
-                    "dimensionality": 1,
-                    "metadata": {"data_origin": value["_ert_kind"]},
-                    "log_scale": key.startswith("LOG10_"),
-                }
-                # response: requests.Response = client.get(
-                #     f"/ensembles/{ensemble['id']}/responses", timeout=self._timeout
-                # )
-                # self._check_response(response)
-                # ensemble.load_response()
-                # for key, value in response.json().items():
-                #     all_keys[key] = {
-                #         "key": key,
-                #         "index_type": "VALUE",
-                #         "observations": value["has_observations"],
-                #         "dimensionality": 2,
-                #         "metadata": value["userdata"],
-                #         "log_scale": key.startswith("LOG10_"),
-                #     }
+                sub_indexes = ensemble.load_parameters(key).get_index("names").to_list()
+                for sub_index in sub_indexes:
+                    all_parameter_sub_keys.add(
+                        (key + ":" + sub_index, value["_ert_kind"])
+                    )
 
-                # response: requests.Response = client.get(
-                #     f"/ensembles/{ensemble['id']}/parameters", timeout=self._timeout
-                # )
-                # self._check_response(response)
-                # for e in response.json():
-                #     key = e["name"]
-                #     all_keys[key] = {
-                #         "key": key,
-                #         "index_type": None,
-                #         "observations": False,
-                #         "dimensionality": 1,
-                #         "metadata": e["userdata"],
-                #         "log_scale": key.startswith("LOG10_"),
-                #     }
+        for key, kind in sorted(list(all_parameter_sub_keys)):
+            all_keys[key] = {
+                "key": key,
+                "index_type": None,
+                "observations": False,
+                "dimensionality": 1,
+                "metadata": {"data_origin": kind},
+                "log_scale": key.startswith("LOG10_"),
+            }
 
         return list(all_keys.values())
 
     def get_all_cases_not_running(self) -> List:
         """Returns a list of all cases that are not running. For each case a dict with
         info about the case is returned"""
-        print("Called: get_all_cases_not_running")
         # Currently, the ensemble information from the storage API does not contain any
         # hint if a case is running or not for now we return all the cases, running or
         # not
@@ -181,7 +166,6 @@ class PlotApi:
         """Returns a pandas DataFrame with the datapoints for a given key for a given
         case. The row index is the realization number, and the columns are an index
         over the indexes/dates"""
-        print("Called: data_for_key", case_name)
 
         if key.startswith("LOG10_"):
             key = key[6:]
@@ -190,33 +174,45 @@ class PlotApi:
 
         ensemble = self._storage.get_ensemble(case["id"])
         try:
-            data = ensemble.load_response(key, tuple(range(ensemble.ensemble_size)))
-            print(data.isel(report_step=0))
-            return data.isel(report_step=0).to_dataframe().drop(columns="report_step").unstack("index")
-        except KeyError:
-            data = ensemble.load_parameters(key)
-
-        print(data)
-        with StorageService.session() as client:
-            response: requests.Response = client.get(
-                f"/ensembles/{case['id']}/records/{key}",
-                headers={"accept": "application/x-parquet"},
-                timeout=self._timeout,
+            main_key, sub_key = (
+                key.split(":", maxsplit=1)
+                if ":" in key
+                else key.split("@", maxsplit=1)
+                if "@" in key
+                else (key, None)
             )
-            self._check_response(response)
-
-            stream = io.BytesIO(response.content)
-            df = pd.read_parquet(stream)
-
+            data = ensemble.load_response(
+                main_key, tuple(range(ensemble.ensemble_size))
+            )
+            if "@" in key:
+                return (
+                    data.sel(report_step=int(sub_key))
+                    .to_dataframe()
+                    .drop(columns="report_step")
+                    .unstack("index")
+                    .droplevel(0, axis=1)
+                )
+            else:
+                return (
+                    data.sel(name=sub_key)
+                    .to_dataframe()
+                    .drop(columns="name")
+                    .unstack("time")
+                    .droplevel(0, axis=1)
+                )
+        except (ValueError, KeyError):
             try:
-                df.columns = pd.to_datetime(df.columns)
-            except (ParserError, ValueError):
-                df.columns = [int(s) for s in df.columns]
-
-            try:
-                return df.astype(float)
-            except ValueError:
-                return df
+                tail, head = key.split(":", maxsplit=1)
+                data = (
+                    ensemble.load_parameters(tail, var="transformed_values")
+                    .to_dataframe()
+                    .unstack("names")
+                    .droplevel(0, axis=1)[[head]]
+                )
+                data.columns = [0]
+                return data
+            except (ValueError, KeyError):
+                return pd.DataFrame()
 
     def observations_for_key(self, case_name, key):
         """Returns a pandas DataFrame with the datapoints for a given observation key
@@ -224,44 +220,51 @@ class PlotApi:
         is a multi-index with (obs_key, index/date, obs_index), where index/date is
         used to relate the observation to the data point it relates to, and obs_index
         is the index for the observation itself"""
-        print("Called: observations_for_key")
 
         case = self._get_case(case_name)
         ensemble = self._storage.get_ensemble(case["id"])
-        ensemble.experiment.observations[key]
+        val = None
+        main_key, sub_key = (
+            key.split(":", maxsplit=1) if ":" in key else key.split("@", maxsplit=1)
+        )
+        sub_key = int(sub_key) if "@" in key else sub_key
+        is_obs_summary = False
+        val = None
+        for v in ensemble.experiment.observations.values():
+            is_summary = "time" in list(v.coords.dims.keys())
+            if (
+                v.attrs["response"] == main_key
+                and sub_key in v.coords["name" if is_summary else "report_step"]
+            ):
+                is_obs_summary = is_summary
+                if val is None:
+                    val = v.sel({"name" if is_summary else "report_step": sub_key})
+                else:
+                    val = val.merge(
+                        v.sel({"name" if is_summary else "report_step": sub_key})
+                    )
 
-        with StorageService.session() as client:
-            response = client.get(
-                f"/ensembles/{case['id']}/records/{key}/observations",
-                timeout=self._timeout,
-            )
-            self._check_response(response)
-            try:
-                obs = response.json()[0]
-            except (KeyError, IndexError, JSONDecodeError) as e:
-                raise httpx.RequestError("Observation schema might have changed") from e
-            try:
-                int(obs["x_axis"][0])
-                key_index = [int(v) for v in obs["x_axis"]]
-            except ValueError:
-                key_index = [pd.Timestamp(v) for v in obs["x_axis"]]
-
-            data_struct = {
-                "STD": obs["errors"],
-                "OBS": obs["values"],
-                "key_index": key_index,
-            }
-            return pd.DataFrame(data_struct).T
+        dict_dataset = val.to_dict()
+        data_struct = {
+            "STD": dict_dataset["data_vars"]["std"]["data"],
+            "OBS": dict_dataset["data_vars"]["observations"]["data"],
+            "key_index": val.indexes["time" if is_obs_summary else "index"].to_list(),
+        }
+        return pd.DataFrame(data_struct).T
 
     def history_data(self, key, case=None) -> pd.DataFrame:
         """Returns a pandas DataFrame with the data points for the history for a
         given data key, if any.  The row index is the index/date and the column
         index is the key."""
-        print("Called: history_data")
 
         if ":" in key:
-            head, tail = key.split(":", 2)
-            history_key = f"{head}H:{tail}"
+            head, tail = key.split(":", 1)
+            if ":" in tail:
+                parts = tail.split(":", 1)
+                tail = f"{parts[0]}H:{parts[0]}"
+            else:
+                tail = tail + "H"
+            history_key = f"{head}:{tail}"
         else:
             history_key = f"{key}H"
 
